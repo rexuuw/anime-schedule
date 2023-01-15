2deb076e7bd2f17a5b7d65bf5c7493bcc4f7c572
Rex <menzeljonas@gmx.de>
2023-01-15 02:40:04 +0100

    added check if WatchList exist

--- src/stores/watchList.js
+++ src/stores/watchList.js
@@ -1,5 +1,9 @@
 import { writable } from 'svelte/store';
 
+// Check if WatchList exists in localStorage, if it doesn't create it   
+if (!localStorage.getItem("watchList")) {
+  localStorage.setItem("watchList", JSON.stringify([]));
+} 
 let watchList = JSON.parse(localStorage.getItem("watchList"));
 
 const watchListStore = writable(watchList);
