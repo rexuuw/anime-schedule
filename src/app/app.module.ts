9511cd0b812e63736480b051f888fe5ca463bfce
Rex <menzeljonas@gmx.de>
2023-12-22 04:33:27 +0100

    added Crunchyroll links for series

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SchedulePageComponent } from './pages/schedule-page/schedule-page.component';
 import { ScheduleEntryComponent } from './components/schedule-entry/schedule-entry.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { ScheduleEntryComponent } from './components/schedule-entry/schedule-ent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
